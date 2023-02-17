7176bc96c891a05d7b357b2b9b3ba2778ef3858a
CarolinaMacdian <115894648+CarolinaMacdian@users.noreply.github.com>
2023-02-17 15:01:22 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,5 +3,37 @@
 import "./App.css";
 
 export default function App() {
-  return <div className='App'>Weather App</div>;
+  return (
+    <div className='App'>
+      Weather App
+      <footer>
+        <p>
+          This project was coded by{" "}
+          <a
+            href='https://linkedin.com/in/carolinamacdian'
+            class='link'
+            target='_blank'
+            rel='noreferrer'>
+            Carolina Macdian
+          </a>
+          , is{" "}
+          <a
+            href='https://github.com/CarolinaMacdian/weather-app-react'
+            class='link'
+            target='_blank'
+            rel='noreferrer'>
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href='https://taupe-cheesecake-ec5e83.netlify.app/'
+            class='link'
+            target='_blank'
+            rel='noreferrer'>
+            hosted on Netlify
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
 }
